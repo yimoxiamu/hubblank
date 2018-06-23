f9ce27d9b801026993e06193c3e6f870948e6a97
yimoxiamu <2959623954@qq.com>
2018-06-23 13:37:04 +0800

    hubblank

---
+++ hubblank/src/main/java/com/hub/hubblank/HubblankApplication.java
@@ -0,0 +1,12 @@
+package com.hub.hubblank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubblankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubblankApplication.class, args);
+    }
+}
---
+++ hubblank/src/test/java/com/hub/hubblank/HubblankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hub.hubblank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HubblankApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
